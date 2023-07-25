6d1ffe5f75eddb8364ff91a49802d2c420b6cb66
Sameer Mirza <srmirza14@gmail.com>
2023-07-24 21:15:46 -0400

    adjusted resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container px-5 py-5 mx-auto text-center">
-        <h4>
+        <h4 className="pb-2">
           Made with{" "}
           <span
             className="emoji"
@@ -15,6 +15,25 @@ export default function Footer() {
             ❤️
           </span>
         </h4>
+        <ul className="mt-3 text-lg font-medium text-white-900 dark:text-gray-900 sm:mt-0">
+          <li>
+            <a
+              href="https://github.com/kankanrr/"
+              target="_blank"
+              className="mr-4 hover:underline md:mr-6 "
+            >
+              Github
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sameerrmirza/"
+              className="mr-4 hover:underline md:mr-6 "
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
-import { testimonials } from "../data";
 
 export default function Resume() {
   return (
@@ -11,25 +10,25 @@ export default function Resume() {
           Resume
         </h1>
         <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto pb-10">
-          Download a copy of my resume by clicking the card below.
+          Feel free to download a copy of my resume
         </p>
-        <div className="flex flex-wrap -m-4 justify-center">
-          <a href="" key="./resume.png" className="sm:w-1/2 w-100 p-4">
-            <div className="flex relative justify-center">
-              {/* <img
-                alt="gallery"
-                className="absolute inset-0 w-full h-full object-cover object-center border-double border-4 border-sky-500"
-                src="./resume.png"
-              /> */}
-              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-100 hover:bg-purple-900">
+        <div className="flex -m-4 justify-center">
+          <a
+            href=""
+            target="_blank"
+            key="./resume.png"
+            className="sm:w-1/2 w-100 p-4"
+          >
+            <div className="flex">
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-100 hover:bg-gray-900">
                 <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                  Sameer Mirza - Resume
+                  LMB's Resume
                 </h2>
                 <h1 className="title-font text-lg font-medium text-white mb-3">
-                  Click to see document
+                  Click to view full document
                 </h1>
                 <p className="leading-relaxed">
-                  A google docs link to view my full resume
+                  Resume hosted on google docs for ease of access
                 </p>
               </div>
             </div>
